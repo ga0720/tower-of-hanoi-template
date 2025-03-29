6867dc64a91f4e588645a46ab1956f06172ffaac
ga0720 <gauravi.agr5@gmail.com>
2025-03-28 20:28:22 -0700

    Update TowerSolver.java

--- TowerSolver.java
+++ TowerSolver.java
@@ -7,10 +7,9 @@ public class TowerSolver {
         // Nothing to do here
     }
 
-    public void solve(TowerModel model)
-    {
+    public void solve(TowerModel model) {
         this.model = model;
-        // Call the missing solve method (not this one)
+        solve(model.getHeight(), 0, 2, 1);  // Move all disks from tower 0 to tower 2, using tower 1 as auxiliary
     }
 
     // Create an overloaded solve(...) method
@@ -18,5 +17,14 @@ public class TowerSolver {
     //
     // [ solve method here]
     //
-
+    private void solve(int n, int source, int destination, int auxiliary) {
+        if (n == 1) {
+            model.move(source, destination);
+            return;
+        }
+        
+        solve(n - 1, source, auxiliary, destination); // Move n-1 disks to auxiliary
+        model.move(source, destination);              // Move the nth (largest) disk to destination
+        solve(n - 1, auxiliary, destination, source); // Move n-1 disks from auxiliary to destination
+    }
 }
